b87aebffad7e7f9fc27a6381b601682701f6a4f4
omar99elnemr <63202153+omar99elnemr@users.noreply.github.com>
2025-01-15 17:42:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ generic_url = st.text_input("Enter URL (YouTube video or website):", key="url_in
 @st.cache_resource
 def get_llm():
     return ChatGroq(
-        model="gemma-7b-it",
+        model="google/gemma-2-9b-it",
         groq_api_key=st.secrets['GROQ_API_KEY']
     )
 
