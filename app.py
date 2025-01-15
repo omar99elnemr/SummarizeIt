8cae7c89b97d9fa6ff9a72ef6bd6612a32d80a75
omar99elnemr <63202153+omar99elnemr@users.noreply.github.com>
2025-01-15 17:23:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,8 @@ def summarize_content(url):
                 ssl_verify=False,
                 headers={
                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
-                }
+                },
+                mode="elements"
             )
         
         docs = loader.load()
